741ea2d7dfab1011b9ca2e9674a355dd5ba44336
Kavinkumar <kavinkumar7834@gmail.com>
2024-01-31 10:39:17 +0530

    intial commit

--- config/serverConfig.js
+++
@@ -1,9 +0,0 @@
-module.exports.ServerPort = 8083;
-let ENV = 'DEV';
-
-module.exports.base_url = '';
-if (ENV == 'UAT') {
-    module.exports.base_url = '';
-} else if (ENV == 'PROD') {
-    module.exports.base_url = '';
-}
